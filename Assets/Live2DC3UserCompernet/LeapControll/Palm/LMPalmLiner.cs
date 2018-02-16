cecd5d94bc9f397eb961657af494fe5aea7dca4d
komine kenitirou <komine@cybernoids.jp>
2018-02-16 22:08:07 +0900

    手首制御枠作成 制御系まだ

--- Assets/Live2DC3UserCompernet/LeapControll/Palm/LMPalmLiner.cs
+++ Assets/Live2DC3UserCompernet/LeapControll/Palm/LMPalmLiner.cs
@@ -10,11 +10,22 @@ namespace Ganeesyan.Cubism3Compornets
 		public Quaternion handQ;
 		public Vector3 handEl;
 		public float handRoll;
+
+        public List<palmRollingParameter> list;
+
 		// Use this for initialization
 		public override void Refresh()
 		{
 			base.Refresh();
-		}
+
+            foreach (var e in model.GetComponentsInChildren<palmRollingParameter>())
+            {
+                if ( e.isLeft == isLeft)
+                {
+                    list.Add(e);
+                }
+            }
+        }
 
 		// Update is called once per frame
 		void LateUpdate()
@@ -23,8 +34,11 @@ namespace Ganeesyan.Cubism3Compornets
 			if (hand != null)
 			{
 				handQ = hand.Rotation.ToQuaternion();
-				handEl = handQ.eulerAngles;
-				handQ.ToAngleAxis(out handRoll,out handEl);
+                list.ForEach(x => x.setQuaternion(handQ));
+
+                handEl = hand.Direction.ToVector3();//handQ.eulerAngles;
+                var target = Vector3.forward;
+				handQ.ToAngleAxis(out handRoll,out target);
 			}
 		}
 	}
